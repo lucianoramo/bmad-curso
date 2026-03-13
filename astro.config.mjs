0b45ae2dffb900d23b3ecf70335f1f8fcacad73a
Luciano Ramo <lucianoramo76@gmail.com>
2026-03-13 13:51:46 -0300

    feat: hide left sidebar with custom css

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'BMAD Curso',
+      customCss: ['./src/styles/custom.css'],
       defaultLocale: 'root',
       locales: {
         root: { label: 'Português (Brasil)', lang: 'pt-BR' },
